ae229393db3d1d32df7629be1bd20f57ae9fd3ac
Dereck Sebastián Muñoz Castiblanco <dsmunozc@indracompany.com>
2022-07-28 20:56:03 -0500

    74. Creando una lista de categorias

--- src/GifExperApp.jsx
+++ src/GifExperApp.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+
 const GifExperApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  // no es recomendable colocar un hook dentro de un if, esto va contra los principios de un hook
+
+  console.log(categories);
+
   return (
     <>
       <h1>GifExperApp</h1>
+
+      <ol>
+        {/* El método map nos ayuda a recorrer todos los elementos de una lista  */}
+        {categories.map( category => {
+          return <li key={category}> {category}</li>
+        })}
+      </ol>
     </>
+
+
   )
 }
 
