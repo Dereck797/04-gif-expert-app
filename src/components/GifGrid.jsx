27bb87ef0367d3807fd315b0330b6b6b9c78ba5a
Dereck Sebastián Muñoz Castiblanco <dsmunozc@indracompany.com>
2022-07-31 22:59:37 -0500

    84. Mostar los titulos de las imagenes, mi solución

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
     useEffect(() => {
-        getGifs(category);
+        getGifs(category)
+            .then(newImages => setImages(newImages));
     }, [ ]); 
 
     return (
         <>
             <h3> {category} </h3>
-
+            <ol>
+                {
+                    images.map( image => <li key={image.id}> {image.title} </li> )
+                }
+            </ol>
         </>
     )
 }
