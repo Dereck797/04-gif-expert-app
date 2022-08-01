7e0b564d4f3cc4145bfd7b93061b7386aae23fdf
Dereck Sebastián Muñoz Castiblanco <dsmunozc@indracompany.com>
2022-07-31 22:05:13 -0500

    GifGrid - Nuevo Componente

--- src/GifExperApp.jsx
+++ src/GifExperApp.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
 
 export const GifExperApp = () => {
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-  // no es recomendable colocar un hook dentro de un if, esto va contra los principios de un hook
-  // console.log(categories);
-
-  const onAddCategory = ( newCategory ) => {
-    if(categories.includes(newCategory)) return;
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
     setCategories([newCategory, ...categories]);
 
   }
@@ -17,20 +15,13 @@ export const GifExperApp = () => {
     <>
       <h1>GifExperApp</h1>
 
+      <AddCategory onNewCategory={onAddCategory} />
 
-      {/* Existe otra manera de implementar la funcionalidad y es recibiendo la información del componente hijo para que el componente lo renderize  */}
-      <AddCategory
-        // setCategories={setCategories} categories={categories}
-        onNewCategory={ onAddCategory }
-      />
-
-
-      <ol>
-        {/* El método map nos ayuda a recorrer todos los elementos de una lista  */}
-        {categories.map(category => {
-          return <li key={category}> {category}</li>
-        })}
-      </ol>
+      {
+        categories.map(category => (
+          <GifGrid key={category} category={category} />
+        ))
+      }
     </>
 
 
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -4,17 +4,13 @@ export const AddCategory = ({onNewCategory}) => {
 
     const [inputValue, setInputValue] = useState('One Punch');
 
-    // se desestructura el target del event
     const onInputChange = ({ target }) => {
-        // console.log(event.target.value);
         setInputValue(target.value);
-        console.log(inputValue);
     }
 
     const onSumit = (event) => {
         event.preventDefault();
         if(inputValue.trim().length <= 1) return;
-        // setCategories( categories => [inputValue ,...categories]);
         onNewCategory(inputValue)
         setInputValue('');
     }
@@ -25,10 +21,7 @@ export const AddCategory = ({onNewCategory}) => {
                 type="text"
                 placeholder="Buscar gifs"
                 value={inputValue}
-                // onChange={ (event) =>  onInputChange(event) }
-                // en el onchange siempre estoy recibiendo un evento, por ende se puede eliminar, el set del event
                 onChange={onInputChange}
-
             />
         </form>
 
